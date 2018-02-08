da0faa017aeb87eafbcf70a3ccb159ba1893ca7b
rizkisinatra13 <36265016+rizkisinatra13@users.noreply.github.com>
2018-02-09 02:05:41 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -3,30 +3,21 @@
 copyright @ medantechno.com
 Modified @ Farzain - zFz
 2017
-
 */
-
 require_once('./line_class.php');
 require_once('./unirest-php-master/src/Unirest.php');
-
 $channelAccessToken = '2CcN2cTTvxp1pRgAnjjjQTjZPbGFZBY4OvY6Hwrs2UueCSET5uuSz3asFanQ+myHxvxjPwFqMyMJS++SJG51VQ4TH4LCWXRcKzf/8rWmJW+lhqN6InztHj2+bi1OzcVhyweaqIL+DHO6XCBB7B6mJwdB04t89/1O/w1cDnyilFU='; //sesuaikan 
 $channelSecret = 'a6248f988dc6b4d26ee61e4f13bc72c5';//sesuaikan
-
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
-
 $userId 	= $client->parseEvents()[0]['source']['userId'];
 $groupId 	= $client->parseEvents()[0]['source']['groupId'];
 $replyToken = $client->parseEvents()[0]['replyToken'];
 $timestamp	= $client->parseEvents()[0]['timestamp'];
 $type 		= $client->parseEvents()[0]['type'];
-
 $message 	= $client->parseEvents()[0]['message'];
 $messageid 	= $client->parseEvents()[0]['message']['id'];
-
 $profil = $client->profil($userId);
-
 $pesan_datang = explode(" ", $message['text']);
-
 $command = $pesan_datang[0];
 $options = $pesan_datang[1];
 if (count($pesan_datang) > 2) {
@@ -35,7 +26,6 @@ if (count($pesan_datang) > 2) {
         $options .= $pesan_datang[$i];
     }
 }
-
 #-------------------------[Function]-------------------------#
 function cuaca($keyword) {
     $uri = "http://api.openweathermap.org/data/2.5/weather?q=" . $keyword . ",ID&units=metric&appid=e172c2f3a3c620591582ab5242e0e6c4";
@@ -50,37 +40,15 @@ function cuaca($keyword) {
 	$result .= $json['weather']['0']['description'];
     return $result;
 }
-function shalat($keyword) {
-    $uri = "https://time.siswadi.com/pray/" . $keyword;
-    $response = Unirest\Request::get("$uri");
-    $json = json_decode($response->raw_body, true);
-    $result = "Jadwal Shalat Sekitar ";
-	$result .= $json['location']['address'];
-	$result .= "\nTanggal : ";
-	$result .= $json['time']['date'];
-	$result .= "\n\nShubuh : ";
-	$result .= $json['data']['Fajr'];
-	$result .= "\nDzuhur : ";
-	$result .= $json['data']['Dhuhr'];
-	$result .= "\nAshar : ";
-	$result .= $json['data']['Asr'];
-	$result .= "\nMaghrib : ";
-	$result .= $json['data']['Maghrib'];
-	$result .= "\nIsya : ";
-	$result .= $json['data']['Isha'];
-    return $result;
-}
 #-------------------------[Function]-------------------------#
-
 # require_once('./src/function/search-1.php');
 # require_once('./src/function/download.php');
 # require_once('./src/function/random.php');
 # require_once('./src/function/search-2.php');
 # require_once('./src/function/hard.php');
-
 //show menu, saat join dan command /menu
 if ($type == 'join' || $command == '/menu') {
-    $text = "Halo Kak ^_^\nAku Bot SANGE ONLINE VVIP, ketik keysov untuk keyword";
+    $text = "Halo Kak ^_^\nAku Bot SANGE ONLINE VVIP, ketik /keysov untuk keyword";
     $balas = array(
         'replyToken' => $replyToken,
         'messages' => array(
@@ -91,9 +59,7 @@ if ($type == 'join' || $command == '/menu') {
         )
     );
 }
-
 	    if ($command == '/cuaca') {
-
         $result = cuaca($options);
         $balas = array(
             'replyToken' => $replyToken,
@@ -105,27 +71,10 @@ if ($type == 'join' || $command == '/menu') {
             )
         );
     }
-else if($message['type']=='text') {
-	    if ($command == '/shalat') {
-        $result = shalat($options);
-        $balas = array(
-            'replyToken' => $replyToken,
-            'messages' => array(
-                array(
-                    'type' => 'text',
-                    'text' => $result
-                )
-            )
-        );
-    }
-
 if (isset($balas)) {
     $result = json_encode($balas);
 //$result = ob_get_clean();
-
     file_put_contents('./balasan.json', $result);
-
-
     $client->replyMessage($balas);
 }
 ?>
